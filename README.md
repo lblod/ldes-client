38542c97489f782db7f23e3ea722268576d22c11
karel kremer <karel.kremer@redpencil.io>
2024-08-07 08:19:25 +0200

    initial repo setup

---
+++ README.md
@@ -0,0 +1,6 @@
+# LDES client
+
+A simple (for now?) LDES client that aims for efficiency and customizability
+
+> [!CAUTION]
+> This service is under construction and is not ready to be used in a production environment
---
+++ app.ts
@@ -0,0 +1,30 @@
+import { app } from 'mu';
+import express, { Request, ErrorRequestHandler } from 'express';
+import bodyParser from 'body-parser';
+
+
+app.use(
+  bodyParser.json({
+    limit: '500mb',
+    type: function (req: Request) {
+      return /^application\/json/.test(req.get('content-type') as string);
+    },
+  }),
+);
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', async (_req, res) => {
+  res.send({ status: 'ok' });
+});
+
+
+const errorHandler: ErrorRequestHandler = function (err, _req, res, _next) {
+  // custom error handler to have a default 500 error code instead of 400 as in the template
+  res.status(err.status || 500);
+  res.json({
+    errors: [{ title: err.message, description: err.description?.join('\n') }],
+  });
+};
+
+app.use(errorHandler);
