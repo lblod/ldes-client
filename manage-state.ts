6bdc87bbdfc3692b024cb89bdc6b6bd1286157b0
Elena Poelman <elena.poelman@redpencil.io>
2025-11-21 10:24:40 +0100

    gatherStateInfo: use correct https-ldes namespace

--- manage-state.ts
+++ manage-state.ts
@@ -28,10 +28,16 @@ export const runningState: RunningState = {
 
 export async function gatherStateInfo(currentPage): Promise<StateInfo> {
   const lastTime = await querySudo(
-    `
+    /* sparql */ `
     SELECT ?stream ?lastTime ?nextPage WHERE {
       GRAPH ${sparqlEscapeUri(WORKING_GRAPH)} {
-        ?stream a <http://w3id.org/ldes#EventStream> .
+        {
+          ?stream a <http://w3id.org/ldes#EventStream> .
+        }
+        UNION
+        {
+          ?stream a <https://w3id.org/ldes#EventStream> .
+        }
         OPTIONAL {
           ?stream <https://w3id.org/tree#member> ?versionedMember.
           ?versionedMember ${sparqlEscapeUri(environment.getTimePredicate())} ?lastTime.
